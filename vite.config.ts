a18bb71d24de64f6614f96c91342d0b2a714bc33
Nathan Malicki <ntm5579@gmail.com>
2026-09-04 18:46:31 -0400

    robots and sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import Sitemap from 'vite-plugin-sitemap'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    Sitemap({ hostname: 'https://example.com' }),
   ],
 })
