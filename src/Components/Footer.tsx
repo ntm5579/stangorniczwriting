22d52409d0f60d74902faff3a1dc8cd09ab837b8
Nathan Malicki <ntm5579@gmail.com>
2026-09-04 19:25:10 -0400

    router change and more inputs

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,12 +1,14 @@
 import './Footer.css';
+import asja from '../assets/organizationLogos/asja.png';
+import awp from '../assets/organizationLogos/awp.png';
 
 function Footer(){
   return (
     <footer className="footer">
       <p>Contact: <a href="mailto:sjgornicz@gmail.com">sjgornicz@gmail.com</a></p>
       <div className="organization-logos">
-          <img height="75px" src="src/assets/organizationLogos/asja.png"></img>
-          <img height="75px" src="src/assets/organizationLogos/awp.png"></img>
+          <img height="75px" src={asja}></img>
+          <img height="75px" src={awp}></img>
       </div>
     </footer>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import PublishedArticles from './PublishedArticles.tsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {path: "/", element: <App />},
   {path: "/published-work", element: <PublishedArticles />},
-], { basename: '/stangorniczwriting' });
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
