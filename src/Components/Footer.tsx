42e31353dbe2b03b5b5199f24de37482891f3b4b
Nathan Malicki <ntm5579@gmail.com>
2026-09-04 11:18:02 -0400

    font change and footer styling

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,8 +2,8 @@ function Footer(){
   return (
     <footer style={{textAlign: 'center', padding: '1em', background: '#000'}}>
         <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-            <p>Contact: <a href="mailto:sjgornicz@gmail.com">sjgornicz@gmail.com</a></p>
-            <div>
+            <p style={{color: '#fff'}} >Contact: <a  style= {{color: '#fff'}} href="mailto:sjgornicz@gmail.com">sjgornicz@gmail.com</a></p>
+            <div style={{display: 'inline-flex', gap: '1em', alignItems: 'center'}}>
                 <img src="src/assets/organizationLogos/asja.png"></img>
                 <img src="src/assets/organizationLogos/awp.png"></img>
             </div>
