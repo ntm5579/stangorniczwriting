607d3783d28282e336b91cb28c0c700eea95c73c
Nathan Malicki <ntm5579@gmail.com>
2026-09-04 15:47:41 -0400

    article sorting, mobile styling tune ups

--- src/PublishedArticles.tsx
+++ src/PublishedArticles.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Nav from './Components/Nav.tsx'
 import { articles } from './constants'
 import Article from './Components/Article.tsx'
@@ -5,11 +6,23 @@ import Footer from './Components/Footer'
 import './Components/Article.css'
 
 function PublishedArticles() {
+    const [sortBy, setSortBy] = useState('publication')
+    const sortedByDate= [...articles].sort((a, b) => new Date(b.publicationDate).getTime() - new Date(a.publicationDate).getTime())
+
     return (
         <div className="page-shell" style={{ backgroundColor: '#FFF8F1' }}>
             <Nav currentPage="published-work"/>
-            <h1 style={{ textAlign: 'center', fontSize: '36px', fontWeight: '800' }}>Published Work</h1>
-            {articles.map((article, index) => (
+            <h1 className="page-title">Published Work</h1>
+            <div className="sort-section">
+                <p>Sort Articles:</p>
+                <button onClick={() => setSortBy('publication')}>
+                    by Publication
+                </button>
+                <button onClick={() => setSortBy('date')}>
+                    by Date
+                </button>
+            </div>
+            {(sortBy === 'publication' ? articles : sortedByDate).map((article, index) => (
                 <Article
                     key={index}
                     image={article.image}
