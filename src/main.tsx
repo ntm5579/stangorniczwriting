a5d583161370d2c700d26f2f0706564d42758d89
Nathan Malicki <ntm5579@gmail.com>
2026-09-04 19:05:37 -0400

    base url fix

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import PublishedArticles from './PublishedArticles.tsx'
 const router = createBrowserRouter([
   {path: "/", element: <App />},
   {path: "/published-work", element: <PublishedArticles />},
-]);
+], { basename: '/stangorniczwriting' });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
